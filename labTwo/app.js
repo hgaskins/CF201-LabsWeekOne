2e6f1e2bf6dfb5406226d74bf229f07f82661dae
hannah gaskins <hnag409@gmail.com>
2016-03-15 16:30:26 -0700

    inital stages of the labTwo process. added HTML and JS to get started

---
+++ labTwo/app.js
@@ -0,0 +1,11 @@
+//CF201
+//labTwo-aboutMe
+
+var userName = prompt("Hello! What is your name?");
+console.log("This user's name is " + userName);
+
+
+alert("Hey there " + userName + " let's play a narcissistic game about ME! Go ahead and click OK there :)" );
+
+var userResponseOne = prompt("Was I born in Wisconsin?");
+console.log("The user's respons is " + userResponseOne);
