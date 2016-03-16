a2a46a9875514d7634f3318b65c5031c4a4e44e7
hannah gaskins <hnag409@gmail.com>
2016-03-16 11:46:33 -0700

    added an additional question, and comments about where the code could be more DRY with additional JS function at top of file

--- labTwo/app.js
+++ labTwo/app.js
@@ -8,13 +8,16 @@ by the id i placed within the html to then add in a textContent
 method by the one variable declared.
 */
 
-function findId() {
-  document.getElementById();
-}
+/* function to return document method "getElementById" in DRY*/
+// function findId(id) {
+//   return document.getElementById(id);
+// }
 
 var userName = prompt("Hello! What is your name?");
 console.log("This user's name is " + userName);
+//use findId function here
 var one = document.getElementById("tagName");
+findId("tagName");
 one.textContent = "Hey there " + userName + "!";
 
 alert("Hey there " + userName + " let's play a narcissistic game about ME! Go ahead and click OK there :)" );
@@ -22,7 +25,9 @@ alert("Hey there " + userName + " let's play a narcissistic game about ME! Go ah
 /* the first question to the user with prompt to user, logs the user response to the console, and then provides a conditional response based on what the user responded with */
 var userResponseOne = prompt("Was I born in Wisconsin?");
 console.log("The user's respons is " + userResponseOne);
+//use findId function here
 var two = document.getElementById("tagResponse");
+var two = findId("tagResponse", userResponseOne);
 
 if(userResponseOne.toLowerCase() === "n" || userResponseOne.toLowerCase() === "no") {
   alert("Correct! Minnesota is where I was born :)");
@@ -41,6 +46,7 @@ if(userResponseOne.toLowerCase() === "n" || userResponseOne.toLowerCase() === "n
 
 var userResponseTwo = prompt("What sport did I play growing up?");
 console.log("The user's response is " + userResponseTwo);
+//use findId function here
 var three = document.getElementById('tagSport');
 
 if (userResponseTwo.toLowerCase() === "hockey" || userResponseTwo.toLowerCase() === "soccer" || userResponseTwo.toLowerCase() === "tennis") {
@@ -51,6 +57,36 @@ if (userResponseTwo.toLowerCase() === "hockey" || userResponseTwo.toLowerCase() 
   alert("Nah I never played that!");
   console.log("the user's response was " + userResponseTwo + " which is incorrect");
   three.textContent = "That is a fun sport, but nope I didn't play it!";
+}
+
+var userResponseThree = prompt("Did I ever live in Chile?");
+console.log("The user's response is " + userResponseThree);
+//use findId function here
+var four = document.getElementById("tagChile");
+
+if (userResponseThree.toLowerCase() === "y" || userResponseThree.toLowerCase() === "yes") {
+  alert("correct");
+  console.log("The user answered correctly");
+  four.textContent = "Nicely done!"
+} else {
+  alert("Come on! Go read my bio - it's really short I promise :)");
+  console.log("the user answered incorrectly and doesn't pay attentin or thinks my life story is boring");
+  four.textContent = "Oh my my, you have learned nothing about me!"
+}
+
+var userResponseFour = prompt("Do I play an instrument?");
+console.log("The user said " + userResponseFour);
+// use findId function here
+var five = document.getElementById("tagMusic");
+
+if (userResponseFour.toLowerCase() === "y" || userResponseFour.toLowerCase() === "yes" || userResponseFour.toLowerCase() == "ya") {
+  alert("yes! you are correct :)");
+  console.log("The user answered incorrectly");
+  five.textContent = "You know so much about me already. Kinda creepo!"
+} else {
+  alert("I do play an instrument! Two in fact ");
+  console.log("the user answered incorrectly");
+  five.textContent = "You are a lost cause my friend.";
 }
 
 var theEnd = alert("thanks for playing!");
