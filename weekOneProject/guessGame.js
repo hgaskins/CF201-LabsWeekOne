e67318ee48db15f1dab1ccb7403429f5bc73c38f
Kate Sowles <katesowles@gmail.com>
2016-03-18 11:09:14 -0700

    adding changes to game.html, guessgame.js, and adding our TestingFunction folder where we figured this shit out

--- weekOneProject/guessGame.js
+++ weekOneProject/guessGame.js
@@ -1,34 +1,37 @@
-// var userResponseTwo = prompt("What sport did I play growing up?");
-// console.log("The user's response is " + userResponseTwo);
-// //use findId function here
-// var three = document.getElementById('tagSport');
-//
-// if (userResponseTwo.toLowerCase() === "hockey" || userResponseTwo.toLowerCase() === "soccer" || userResponseTwo.toLowerCase() === "tennis") {
-//   alert("correct!");
-//   console.log("the user said " + userResponseTwo + " which is correct!");
-//   three.textContent = "You pay attention! ";
-// } else {
-//   alert("Nah I never played that!");
-//   console.log("the user's response was " + userResponseTwo + " which is incorrect");
-//   three.textContent = "That is a fun sport, but nope I didn't play it!";
-// }
+// get rid of this when we merge project back into app.js
+var score = 0;
 
-var userResponseTwo = prompt("What sport did I play growing up?");
-var sports = ["hockey", "soccer", "tennis", "lacross"];
-var three = document.getElementById('tagSport')
+var six = document.getElementById("tagNumberGame");
+var age = 28;
 
-for (i = 0; i < sports.length; i++) {
-  //debugging purposes
-  console.log(sports[i]);
-  //check if answer user gives is anything in Sports array
-  if (userResponseTwo === sports[i]) {
-    console.log("user answered sports guessing game correcly!");
-    three.textContent = "You got it right!";
-    alert("Correct!");
-    break;
-  } else if (i === sports.length - 1) {
-    console.log("user was wrong with sports guessing game");
-    three.textContent = "Wrong, wrong, so wrong!";
-    alert("WRONG!");
+var counter = 0;
+function guessy() {
+  while (counter < 5) {
+    //ask q
+    var userGuess = parseInt(prompt("How old am I? take a guess :)"));
+    //interpret response
+    if (userGuess === age) {
+      //if they get it right
+      score++;
+      six.textContent = "CORRECT! I am 28!";
+      console.log("the user got my age correct!");
+      counter = 5;
+      break;
+    } else if (userGuess > 28) {
+      //if too high
+      six.textContent = "Come on. Too high!";
+      console.log("the user guess is too high");
+      counter++;
+    } else if (userGuess < 28) {
+      //if too low
+      six.textContent = "Too Low!";
+      console.log("the user guess is too low");
+      counter++;
+    } else {
+      //if NaN etc
+      six.textContent = "Enter a number!";
+      counter++;
+    }
   }
 }
+guessy();
